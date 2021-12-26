295fb1266456bbc00a104b81c49342fd162ecc34
Eros <eroslbac123456@gmail.com>
2021-12-26 06:42:03 -0500

    Primer

---
+++ src/context/AppReducer.js
@@ -0,0 +1,16 @@
+export default function addReducer(state, action) {
+  console.log(state, action)
+
+  switch (action.type) {
+    case 'ADD_TASK':
+      return { tasks: [...state.tasks, action.payload] }
+
+      case "DELETE_TASK":
+        return{
+          tasks:[]
+        }
+
+    default:
+      break
+  }
+}
---
+++ src/context/GlobalContext.js
@@ -0,0 +1,35 @@
+import { createContext, useReducer } from 'react'
+import appReducer from './AppReducer'
+
+const initialState = {
+  tasks: [
+    {
+      id: '1',
+      title: 'title one',
+      description: 'some description',
+      done: 'false',
+    },
+    {
+      id: '2',
+      title: 'title two',
+      description: 'some description two ',
+      done: 'false ',
+    },
+  ],
+}
+
+export const GlobalContext = createContext(initialState)
+
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(appReducer, initialState)
+
+  const addTask = (task) => {
+    console.log(task)
+    dispatch({ type: 'ADD_TASK', payload: { id: 1, title: 'nuevo' } })
+  }
+  return (
+    <GlobalContext.Provider value={{ ...state, addTask }}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
